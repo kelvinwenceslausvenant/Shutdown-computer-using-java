c3cfffb52515d4dabf67d915ce55ccc431e0fab0
kelvinwenceslausvenant <157791058+kelvinwenceslausvenant@users.noreply.github.com>
2024-05-22 02:50:20 +0300

    Add files via upload

---
+++ shutdown.java
@@ -0,0 +1,17 @@
+import java.io.*;
+public class CodesCracker
+{
+public static void main(String[] args)
+{
+Runtime runtime = Runtime.getRuntime();
+try
+{
+System.out.println("Shutting down the PC after 5 seconds.");
+runtime.exec("shutdown -s -t 5");
+}
+catch(IOException e)
+{
+System.out.println("Exception: " +e);
+}
+}
+}
